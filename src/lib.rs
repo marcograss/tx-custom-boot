e6fd751f60d46ad7ae8d22a4be768e73f187ef92
Marco Grassi <marco.gra@gmail.com>
2022-05-19 20:43:04 +0800

    better error handling

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@
 use binwrite::{BinWrite, WriterOption};
 use sha2::{Digest, Sha256};
 use std::fmt;
-use std::io::{Result, Write};
+use std::fmt::Formatter;
+use std::io::Write;
+use thiserror::Error;
 
 #[derive(BinWrite, Debug, Default)]
 #[binwrite(little)]
@@ -40,6 +42,30 @@ struct BootDatInner {
     pad2: Pad2,
 }
 
+/// Error enum for this crate
+#[derive(Error, Debug, Clone)]
+pub enum Error {
+    /// Error during I/O operations
+    IoError(String),
+    /// Error while converting the hash
+    HashError,
+}
+
+impl std::convert::From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::IoError(err.to_string())
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Error::IoError(s) => write!(fmt, "IO Error: {}", s),
+            Error::HashError => write!(fmt, "Hash Error"),
+        }
+    }
+}
+
 // Workaround because Default and BinWrite don't support arrays of this dimension
 struct Pad2([u8; 0x90]);
 
@@ -56,7 +82,11 @@ impl fmt::Debug for Pad2 {
 }
 
 impl BinWrite for Pad2 {
-    fn write_options<W: Write>(&self, writer: &mut W, options: &WriterOption) -> Result<()> {
+    fn write_options<W: Write>(
+        &self,
+        writer: &mut W,
+        options: &WriterOption,
+    ) -> std::io::Result<()> {
         for item in &self.0 {
             BinWrite::write_options(item, writer, options)?;
         }
@@ -74,7 +104,11 @@ impl fmt::Debug for Sha2 {
 }
 
 impl BinWrite for Sha2 {
-    fn write_options<W: Write>(&self, writer: &mut W, options: &WriterOption) -> Result<()> {
+    fn write_options<W: Write>(
+        &self,
+        writer: &mut W,
+        options: &WriterOption,
+    ) -> std::io::Result<()> {
         for item in &self.0 {
             BinWrite::write_options(item, writer, options)?;
         }
@@ -91,7 +125,7 @@ pub fn get_version() -> &'static str {
 /// from https://gist.github.com/CTCaer/13c02c05daec9e674ba00ce5ac35f5be
 /// but revisited to match https://sx-boot-dat-creator.herokuapp.com/ which works for me
 /// `payload` is a byte array of the payload
-pub fn generate_boot_dat(payload: &[u8]) -> Vec<u8> {
+pub fn generate_boot_dat(payload: &[u8]) -> Result<Vec<u8>, Error> {
     let mut header = BootDatHeader::default();
     header.inner.ident = [
         0x49, 0x6e, 0x73, 0x61, 0x6e, 0x65, 0x20, 0x42, 0x4F, 0x4F, 0x54, 0x00,
@@ -99,21 +133,25 @@ pub fn generate_boot_dat(payload: &[u8]) -> Vec<u8> {
     header.inner.vers = [0x56, 0x31, 0x2E, 0x30];
 
     let stage_2_sha256 = sha256_digest(payload);
-    header.inner.sha2_s2 = Sha2(stage_2_sha256.try_into().unwrap());
+    header.inner.sha2_s2 = Sha2(stage_2_sha256.try_into().map_err(|_| Error::HashError)?);
     header.inner.s2_dst = 0x40010000;
     header.inner.s2_size = payload.len() as u32;
 
     let mut inner_serialized = vec![];
-    header.inner.write(&mut inner_serialized).unwrap();
+    header.inner.write(&mut inner_serialized)?;
 
     let header_inner_sha256 = sha256_digest(inner_serialized.as_slice());
-    header.sha2_hdr = Sha2(header_inner_sha256.try_into().unwrap());
+    header.sha2_hdr = Sha2(
+        header_inner_sha256
+            .try_into()
+            .map_err(|_| Error::HashError)?,
+    );
 
     let mut serialized = vec![];
-    header.write(&mut serialized).unwrap();
+    header.write(&mut serialized)?;
 
     serialized.extend_from_slice(payload);
-    serialized
+    Ok(serialized)
 }
 
 /// Calc sha256 for a byte array
@@ -128,7 +166,7 @@ mod tests {
     use hex_literal::hex;
     #[test]
     fn end_to_end_test() {
-        let generated = super::generate_boot_dat(&[0xa, 0xb, 0xc]);
+        let generated = super::generate_boot_dat(&[0xa, 0xb, 0xc]).unwrap();
         let hash = super::sha256_digest(generated.as_slice());
         assert_eq!(
             hash[..],
